cf7ab81f6dcb2ef75534d898a122d227b77e6f78
Ben_Lipsky <Benlipsky@icloud.com>
2024-05-07 14:58:33 -0400

    ef

--- script.js
+++ script.js
@@ -3,12 +3,28 @@
 const teamMembers = [
     {
         name: 'Tom Brady',
-        position: 'Quaterback',
-        strengths: 'Arm Strength',
+        position: 'Quarterback',
+        strengths: ['GOAT', ' Incredible Arm Strength'],
+        number: '84',
+        skills: [ 'Long Throws', ' Friendliness with Referees'],
+        teams: ['Patriots', ' Buccaneers']
+    },
+    {
+        name: 'Antonio Brown',
+        position: 'Wide Receiver',
+        strengths: [' Insane Hands', ' Insane Speed', ' Insane'],
         number: '12',
-        skills: ['Shovel Passes', 'Long Throws', 'Friendliness with Referees'],
-        biography: 'A AM tim braddyshaw'
-    }
+        skills: ['Elite Playmaker', ' Team Player'],
+        teams:['Steelers', ' Raiders', ' Patriots', ' Buccaneers']
+    },
+    {
+        name: 'Marshawn Lynch',
+        position: 'Running Back',
+        strengths: ['Amazing Maneuverability', ' Unreal Speed'],
+        number: '24',
+        skills: ['Elite Playmaker', ' Outrunner'],
+        teams:['Seahawks', ' Bills', ' Raiders']
+    },
 ];
 
 function generateTeamCards(){
@@ -16,22 +32,45 @@ function generateTeamCards(){
     const teamCardsContainer = document.getElementById('teamCards')
 
     teamMembers.forEach(member => {
-        const card = document.createElement('div')
-        card.classList.add('col-md-4')
+         const card = document.createElement('div')
+         card.classList.add('col-md-4')
+         
+        // Style Card Based on Position
 
-        card.innerHTML= `
-        <div class="card">
-            <div class = "card-header">
-                ${member.name}
+        let backgroundColor
+        switch(member.position.toLowerCase()){
+            case 'quarterback':
+                backgroundColor = "red"
+                break
+            case 'running back':
+                backgroundColor = "blue"
+                break
+            case 'wide receiver':
+                backgroundColor = "yellow"
+                break
+            default:
+                backgroundColor = "grey"
+        }
+        card.style.backgroundColor = backgroundColor
+         card.innerHTML= `
+            <div class="card">
+                <div class = "card-header">
+                    ${member.name}
+                </div>
+                <div class="card-body">
+                    <p><strong>Position:</strong> ${member.position}</p>
+                    <p><strong>Strengths:</strong> ${member.strengths}</p>
+                    <p><strong>Number:</strong> ${member.number}</p>
+                    <p><strong>Skills:</strong> ${member.skills}</p>
+                    <p><strong>Team(s):</strong> ${member.teams}</p>
+
+                </div>
             </div>
-            <div class="card-body">
-                <p><strong>Position:</strong> ${member.position}</p>
-            </div>
-        </div>
-        `
+         
+         `
     teamCardsContainer.appendChild(card)
     });
-};
+}
 
 
-window.onload = generateTeamCards();
+window.onload = generateTeamCards()
