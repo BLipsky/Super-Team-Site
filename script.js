3c2e68f628f560094c00c8e353746ca7d3e0e389
Ben_Lipsky <Benlipsky@icloud.com>
2024-06-10 13:48:50 -0400

    pluh part 6

--- script.js
+++ script.js
@@ -5,18 +5,64 @@ const teamMembers = [
         name: 'Tom Brady',
         position: 'Quarterback',
         strengths: ['GOAT', ' Incredible Arm Strength'],
-        number: '84',
+        number: '12',
         skills: [ 'Long Throws', ' Friendliness with Referees'],
         teams: ['Patriots', ' Buccaneers'],
-        image: 'Tom Brady',
+        image: 'assets/Tom Brady.webp',
+    },
+    {
+        name: 'Aaron Rodgers',
+        position: 'Quarterback',
+        strengths: ['Arm Strength', ' Mobility'],
+        number: '12',
+        skills: ['Elite Play Extender', ' Long Passer'],
+        teams:['Packers', ' Jets',],
+        image: 'assets/Aaron Rodgers.webp'
+    },     
+    {
+        name: 'Peyton Manning',
+        position: 'Quarterback',
+        strengths: ['Durability', ' Greatness'],
+        number: '18',
+        skills: ['Elite Playmaker', ' Long Passer'],
+        teams:['Colts', ' Broncos',],
+        image: 'assets/Peyton Manning.jpg'
     },
     {
         name: 'Antonio Brown',
         position: 'Wide Receiver',
         strengths: [' Insane Hands', ' Insane Speed', ' Insane'],
-        number: '12',
+        number: '84',
         skills: ['Elite Playmaker', ' Team Player'],
-        teams:['Steelers', ' Raiders', ' Patriots', ' Buccaneers']
+        teams:['Steelers', ' Raiders', ' Patriots', ' Buccaneers'],
+        image:'assets/Antonio Brown.webp'
+    },
+        {
+        name: 'Megatron',
+        position: 'Wide Receiver',
+        strengths: [' Insane Hands', ' Insane Speed'],
+        number: '81',
+        skills: ['Elite Playmaker', ' Team Player'],
+        teams:['Lions'],
+        image:'assets/Megatron.webp'
+    },
+    {
+        name: 'Julio Jones',
+        position: 'Wide Receiver',
+        strengths: [' Insane Hands', ' Insane Speed'],
+        number: '11',
+        skills: ['Elite Playmaker', ' Team Player'],
+        teams:['Falcons', ' Eagles', ' Titans', ' Buccaneers'],
+        image:'assets/Julio Jones.jpg'
+    },
+    {
+        name: 'Todd Gurley',
+        position: 'Running Back',
+        strengths: ['Amazing Maneuverability', ' Unreal Speed'],
+        number: '30',
+        skills: ['Elite Playmaker', ' Outrunner'],
+        teams:['Rams', ' Falcons'],
+        image: 'assets/Todd Gurley.png'
     },
     {
         name: 'Marshawn Lynch',
@@ -24,10 +70,47 @@ const teamMembers = [
         strengths: ['Amazing Maneuverability', ' Unreal Speed'],
         number: '24',
         skills: ['Elite Playmaker', ' Outrunner'],
-        teams:['Seahawks', ' Bills', ' Raiders']
+        teams:['Seahawks', ' Bills', ' Raiders'],
+        image: 'assets/Marshawn Lynch.webp'
     },
-];
+    {
+        name: 'Adrian Peterson',
+        position: 'Running Back',
+        strengths: ['Amazing Maneuverability', ' Unreal Speed'],
+        number: '28',
+        skills: ['Elite Playmaker', ' Outrunner'],
+        teams:['Seahawks', ' Bills', ' Raiders'],
+        image: 'assets/Adrian Peterson.jpg'
+    },
+    {
+        name: 'Pete Carroll',
+        position: 'Head Coach',
+        strengths: ['Good Coahing', ' Friendly Guy'],
+        number: 'N/A',
+        skills: ['Elite Playmaker', ' A Few Playoff Appearances'],
+        teams:['Seahwaks'],
+        image: 'assets/Pete Carroll.jpg'
+    },
+        {
+        name: 'Bill Belichick',
+        position: 'Head Coach',
+        strengths: ['Great Cheating', ' Questionable Coaching'],
+        number: 'N/A',
+        skills: ['Elite Playmaker', ' A lot of Playoff Appearances'],
+        teams:['Patriots'],
+        image: 'assets/Bill Belichick.avif'
+    },
+    {
+        name: 'Mike Tomlin',
+        position: 'Head Coach',
+        strengths: ['Amazing Coahing', ' 0 Losing Seasons'],
+        number: 'N/A',
+        skills: ['Elite Playmaker', ' 10/17 Playoff Appearances'],
+        teams:['Steelers'],
+        image: 'assets/Mike Tomlin.jpg'
+    },
 
+];
 function generateTeamCards(){
 
     const teamCardsContainer = document.getElementById('teamCards')
@@ -54,13 +137,15 @@ function generateTeamCards(){
         }
         card.style.backgroundColor = backgroundColor
          card.innerHTML= `
-            <div class="card">
-                <div class="img" src="${member.image} + '.png'>
+         <br>
+            <div class="card c1 m-2">
+                <div>
                 </div>
                 <div class = "card-header">
                     ${member.name}
                 </div>
                 <div class="card-body">
+                    <img class="img-fluid" src="${member.image}">
                     <p><strong>Position:</strong> ${member.position}</p>
                     <p><strong>Strengths:</strong> ${member.strengths}</p>
                     <p><strong>Number:</strong> ${member.number}</p>
@@ -69,7 +154,7 @@ function generateTeamCards(){
 
                 </div>
             </div>
-         
+            <br>
          `
     teamCardsContainer.appendChild(card)
     });
